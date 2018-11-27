cdfda4e10c33163275e3a1f1a2b71884436f411c
dimm4ik <dimosphen@mail.ru>
2018-11-27 23:18:40 +0300

    правим логмодуль 3

--- bitrix/templates/sd/components/realweb/catalog.store.list/.default/result_modifier.php
+++ bitrix/templates/sd/components/realweb/catalog.store.list/.default/result_modifier.php
@@ -312,15 +312,12 @@ if (isset($sT)){
 
 //Получаем список заказав для менеджеров интернет магазина - со статусом отправлен
 //TODO добавить отбор по менеджерам/админ
-var_dump($USER);
-var_dump($arUser);
 if(!$USER->IsAdmin()&&$arUser['UF_DOSTUP']!=0){
 $roM = CSaleOrder::GetList(array($sT[0]=>$sT[1]), array('STATUS_ID'=>'F','DELIVERY_ID'=>3), false, false, array("ID","STATUS_ID","DELIVERY_ID",'ACCOUNT_NUMBER'));
 $orderM = array();
 while($orderOne = $roM->GetNext()){
 	$orderMFull[] = $orderOne;
 }
-var_dump($orderMFull);
 
 foreach($orderMFull as $orderM) {
 	$prM = \Bitrix\Sale\Order::load($orderM['ID']);
@@ -392,8 +389,6 @@ foreach($orderMFull as $orderM) {
 	};
 };
 }
-
-var_dump($arResult['Z-ITEM']);
 
 
 $arResult["ITEMS"] = array();
