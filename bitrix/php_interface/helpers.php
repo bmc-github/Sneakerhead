45e1712842d4fe6b3fd3ff24172db801d5aa87a4
Paul Gorbach <gorbachpaul@gmail.com>
2019-01-16 14:28:56 +0300

    Fix/timestamp (#30)
    * Генерировать для статичных стилей timestamp
    
    * подключил стили через SetAdditionalCSS
    
    * кодировка fix
    
    * порядок подключения стилей fix
    
    * auto versioning
    
    * хеш перенес в get-параметр

--- bitrix/php_interface/helpers.php
+++ bitrix/php_interface/helpers.php
@@ -14,5 +14,6 @@ function autoVersion($file)
         return $file;
 
     $mtime = filemtime($_SERVER['DOCUMENT_ROOT'] . $file);
-    return preg_replace('{\\.([^./]+)$}', ".$mtime.\$1", $file);
+    //return preg_replace('{\\.([^./]+)$}', ".$mtime.\$1", $file);
+    return $file . "?m={$mtime}";
 }
